e86b2bb2815e6728cd059f45d91997189ca2f1cf
Mehraj Hosen <meherajhosen786@gmail.com>
2025-06-03 00:58:19 +0600

    arrivals add

--- src/components/newArrivals/NewArivals.jsx
+++ src/components/newArrivals/NewArivals.jsx
@@ -2,9 +2,15 @@ import React from 'react'
 import Container from '../../layouts/Container'
 import Flex from '../../layouts/Flex'
 import img1 from './../../assets/new1.png'
+import Button from '../../layouts/Button'
 
 const NewArivals = () => {
   const cards= [
+    {
+      img: img1,
+      title: "T-SHIRT WITH TAPE DETAILS",
+      price: "$120"
+    },
     {
       img: img1,
       title: "T-SHIRT WITH TAPE DETAILS",
@@ -25,18 +31,25 @@ const NewArivals = () => {
     <div className='py-[70px]'>
       <Container>
         <h2 className='text-subheading font-black text-center mb-[55px]'>New Arrivals</h2>
-        <Flex>
-          <div className="card md:w-[295px]">
+        <Flex className="justify-start gap-x-[20px]">
+        {
+          cards.map((card)=>(
+            <div className="card md:w-[295px]">
             <div className="img mb-[16px] rounded-[20px] w-full h-[298px] bg-[#F0EEED]"></div>
-              <h4 className='text-subtitle font-bold'>T-SHIRT WITH TAPE DETAILS</h4>
+              <h4 className='text-subtitle font-bold'>{card.title}</h4>
               <span className='my-[8px]'>stars</span>
-              <p className='text-subtitle font-bold'>price</p>
-
-            
-
+              <p className='text-subtitle font-bold'>{card.price}</p>
           </div>
 
+          ))
+        }
+
+
         </Flex>
+        <div className='text-center mt-9'>
+        <Button className="border bg-white !text-black">View More</Button>
+
+        </div>
       </Container>
     </div>
   )
