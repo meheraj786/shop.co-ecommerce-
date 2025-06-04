697675806ef2e2cdbc4ed08e61338eac08229245
Mehraj Hosen <meherajhosen786@gmail.com>
2025-06-04 16:44:22 +0600

    products show

--- src/components/topSelling/TopSelling.jsx
+++ src/components/topSelling/TopSelling.jsx
@@ -1,44 +1,32 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import Container from '../../layouts/Container'
 import Flex from '../../layouts/Flex'
-import img1 from './../../assets/new1.png'
 import Button from '../../layouts/Button'
+import { ProductContext } from '../context/productContenx'
 
 const TopSelling = () => {
-  const cards= [
-  {
-    img: img1,
-    title: "T-SHIRT WITH TAPE DETAILS",
-    price: "$120"
-  },
-  {
-    img: img1,
-    title: "T-SHIRT WITH TAPE DETAILS",
-    price: "$120"
-  },
-  {
-    img: img1,
-    title: "T-SHIRT WITH TAPE DETAILS",
-    price: "$120"
-  },
-  {
-    img: img1,
-    title: "T-SHIRT WITH TAPE DETAILS",
-    price: "$120"
-  },
-]
+  const products= useContext(ProductContext)
+
+const topSellingProducts= products.filter((product)=>product.catagory=="topSelling")
 return (
   <div className='py-[70px] font-secondary'>
     <Container>
       <h2 className='text-subheading-sm md:text-subheading font-black text-center mb-[55px]'>Top Selling</h2>
       <Flex className="md:justify-start justify-center text-center md:text-left md:gap-y-0 gap-y-10 gap-x-[20px]">
       {
-        cards.map((card)=>(
+        topSellingProducts.map((product)=>(
           <div className="card md:w-[295px]">
-          <div className="img mb-[16px] rounded-[20px] w-full h-[298px] bg-[#F0EEED]"></div>
-            <h4 className='text-subtitle-sm md:text-subtitle font-bold'>{card.title}</h4>
-            <span className='my-[8px]'>stars</span>
-            <p className='text-subtitle-sm md:text-subtitle font-bold'>{card.price}</p>
+          <div className="img mb-[16px] rounded-[20px] w-full h-[298px] bg-[#F0EEED]" style={
+            {
+              backgroundImage: `url(${product.img})`,
+              backgroundSize: "cover",
+              backgroundPosition: "center"
+
+            }
+          }></div>
+            <h4 className='text-subtitle-sm md:text-subtitle font-bold'>{product.name}</h4>
+            <span className='my-[8px]'>{product.rating}</span>
+            <p className='text-subtitle-sm md:text-subtitle font-bold'>{product.price}</p>
         </div>
 
         ))
